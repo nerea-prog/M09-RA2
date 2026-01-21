b97e711e597c51616ce7f0cb05fc2b5670ebbc23
nerea-prog <narvaeznerea18@gmail.com>
2026-01-21 22:50:10 +0100

    03-Sleep_i_coet acabat

--- 03-Sleep_i_Coet/src/Coet.java
+++ 03-Sleep_i_Coet/src/Coet.java
@@ -1,3 +1,69 @@
-public class Coet {
+import java.util.Scanner;
 
+public class Coet {
+    private Motor[] motors;
+
+    // Constructor que inicializa los motores
+    public Coet() {
+        motors = new Motor[4];
+        for (int i = 0; i < 4; i++) {
+            motors[i] = new Motor("Motor " + i);
+        }
+    }
+
+    // Inicia todos los motores
+    public void arranca() {
+        for (Motor motor : motors) {
+            motor.start();
+        }
+    }
+
+    // Establece la potencia objetivo para todos los motores
+    public void passaPotencia(int p) {
+        if (p < 0 || p > 10) {
+            System.out.println("Error: la potència ha de ser entre 0 i 10");
+            return;
+        }
+        for (Motor motor : motors) {
+            motor.setPotencia(p);
+        }
+    }
+    
+    // Espera a que todos los motores terminen
+    public void esperarTerminacio() {
+        try {
+            for (Motor motor : motors) {
+                motor.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Coet coet = new Coet();
+        coet.arranca();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            if (sc.hasNextInt()) {
+                int p = sc.nextInt();
+
+                if (p == 0) {
+                    System.out.println("Passant a potència " + p);
+                    coet.passaPotencia(p);
+                    coet.esperarTerminacio();
+                    sc.close();
+                    System.exit(0);
+                }
+
+                System.out.println("Passant a potencia " + p);
+                coet.passaPotencia(p);
+            }
+        }
+    }
 }
+
+
+
--- 03-Sleep_i_Coet/src/Motor.java
+++ 03-Sleep_i_Coet/src/Motor.java
@@ -1,35 +1,62 @@
 public class Motor extends Thread {
-    private int potenciaObjectiu = 0;
     private int potenciaActual = 0;
-    
-    public void setPotenciaObjectiu(int potenciaObjectiu) {
+    private int potenciaObjectiu = 0;
+    private boolean treballant = false;
+
+    // Constructor que inicializa el motor con un nombre
+    public Motor(String nom) {
+        super(nom);
+    }
+     // Establece la potencia objetivo del motor, synchronized para evitar condiciones del hilo
+    public synchronized void setPotencia(int potenciaObjectiu) {
         this.potenciaObjectiu = potenciaObjectiu;
-        if (potenciaObjectiu > potenciaActual) {
-            for (int i = potenciaActual; i < potenciaObjectiu; i++) {
-                this.potenciaActual = i;
-                int segons = (int) (Math.random() * 1000);
-                try{
-                    Thread.sleep(segons);
-                } catch (Exception e){
-                    e.printStackTrace();
+        this.treballant = true;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                if (treballant) {
+                    String accio;
+                    if (potenciaActual < potenciaObjectiu) {
+                        accio = "Incre.";
+                        potenciaActual++;
+                    } else if (potenciaActual > potenciaObjectiu) {
+                        accio = "Decre.";
+                    } else {
+                        accio = "FerRes";
+                        treballant = false;
+                    }
+
+                    if (potenciaActual == potenciaObjectiu && !accio.equals("FerRes")) {
+                        accio = "FerRes";
+                        treballant = false;
+                    }
+
+                    System.out.println(String.format("%s: %s Objectiu: %d Actual: %d",
+                        getName(), accio, potenciaObjectiu, potenciaActual));
+
+                    if (potenciaActual > potenciaObjectiu) {
+                        potenciaActual--;
+                    }
+
+                    if (potenciaActual == potenciaObjectiu && potenciaObjectiu == 0 && !accio.equals("FerRes")) {
+                        System.out.println(String.format("%s: FerRes Objectiu: %d Actual: %d",
+                            getName(), potenciaObjectiu, potenciaActual));
+                        return;
+                    }
+
+                    Thread.sleep(1000 + (int)(Math.random() * 1000));
+                } else {
+                    Thread.sleep(100);
                 }
-                if (potenciaObjectiu == 0 && potenciaActual == 0) {
-                    return;
-                }
-            }
-        } else {
-            for (int i = potenciaActual; i > potenciaObjectiu; i--) {
-                this.potenciaActual = i;
-                int segons = (int) (Math.random() * 1000);
-                try{
-                    Thread.sleep(segons);
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
-                if (potenciaObjectiu == 0 && potenciaActual == 0) {
-                    return;
-                }
+            } catch (InterruptedException e) {
+                break;
             }
         }
-    }    
+    }
 }
+
+
+
