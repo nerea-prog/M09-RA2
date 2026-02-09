ded1b2c8826aba82d3f28fc61fec9efc386f2d1b
nerea-prog <narvaeznerea18@gmail.com>
2026-02-09 16:13:19 +0100

    07-Filosofs practica finalitzada

--- 07-Filosofs/src/Taula.java
+++ 07-Filosofs/src/Taula.java
@@ -34,7 +34,7 @@ public class Taula {
     }
 
     public static void main(String[] args) {
-        Taula taula = new Taula(4);
+        Taula taula = new Taula(5);
         taula.showTaula();
         taula.cridarATaula();
     }
